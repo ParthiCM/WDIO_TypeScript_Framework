1eea5bd1fe12e631e252ef8585d9551755c9d2dc
parthiban murugan <c.m.parthiban1999@gmail.com>
2024-06-14 15:29:32 +0530

    updated package.json for multiple config

--- test/specs/loginTest.ts
+++ test/specs/loginTest.ts
@@ -1,15 +1,19 @@
-// import LoginPage from '../pageobjects/login.page';
-// import Helper from '../pageobjects/CommonHelpers/Helper.ts';
-// import { FormText } from '../Utilities/UserData/FormText.json';
+import LoginPage from '../pageobjects/login.page';
+import Helper from '../pageobjects/CommonHelpers/Helper.ts';
+import { FormText } from '../Utilities/UserData/FormText.json';
 
 
+describe('Orange HRM Application Login Test', () => {
 
-// describe('Orange HRM Application Login Test', () => {
+    it('TC1 - Check User able to login to application without passing the username and password', async () => {
+        await LoginPage.open();
+        await LoginPage.login('', '');
+        await expect(await Helper.verifyPageTitle(FormText.OrangeHRM.toLocaleLowerCase())).toEqual(true);
+    })
 
-//     it('TC1 - Check User able to login to application without passing the username and password', async () => {
-//         await LoginPage.open();
-//         await LoginPage.login('', '');
-//         await expect(await Helper.verifyPageTitle(FormText.OrangeHRM.toLocaleLowerCase())).toEqual(true);
-//     })
-
-// })
+    it('TC2 - Check User able to login to application without passing the username and password', async () => {
+        await LoginPage.open();
+        await LoginPage.login(FormText.userName, '');
+        await expect(await Helper.verifyPageTitle(FormText.OrangeHRM.toLocaleLowerCase())).toEqual(true);
+    })
+})
